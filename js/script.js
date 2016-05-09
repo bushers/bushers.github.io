a5ab1233185eb73734e0a093e7d46929b470e96f
bushers <nbushby01@hotmail.com>
2016-05-09 19:25:36 +0100

    added pasma to portfolio

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
 	showOverlay($('.contact-btn'), $('.overlay-social'));
 	showOverlay($('#portfolio-images-shade-luma'), $('#overlay-shade-luma'));
 	showOverlay($('#portfolio-images-kt-dance'), $('#overlay-kt-dance'));
-	showOverlay($('#portfolio-images-blog'), $('#overlay-blog'));
+	showOverlay($('#portfolio-images-pasma'), $('#overlay-pasma'));
 	showOverlay($('#portfolio-images-homepage'), $('#overlay-homepage'));
 
 
